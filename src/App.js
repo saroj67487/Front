aba8ae4e3f961740fcaf3c7daf80f0f6bc5a87d1
saroj67487 <sarojpoudel67487@gmail.com>
2024-09-12 09:55:33 +0545

    design

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './components/Header'
+import DisplaySecion from './pages/DisplaySecion'
+import Technologies from './pages/Technologies'
+import Intro from './pages/Intro'
+import Footer from './components/Footer'
+
+const App = () => {
+
+
+  return (
+    <div>
+
+
+      <h1>helllo jee</h1>
+
+      <Header />
+      <DisplaySecion />
+      <Technologies />
+      <Intro />
+      <Footer />
+
+
+
+
+    </div>
+  )
+}
+
+export default App
